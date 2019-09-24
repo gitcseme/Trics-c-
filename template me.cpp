c52f25af3a305b1ca4cfd2f1918a1d5fa4bfaa0b
kashuvo <shuvo_CSE23@yahoo.com>
2019-09-24 20:03:57 +0600

    my quick template

---
+++ template me.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FI freopen ("inp.txt", "r", stdin)
+#define FO freopen ("out.txt", "w", stdout)
+#define FOR(v) for(int i : v) {printf("%d ", i);} printf("\n");
+#define what_is(x) cerr << #x << " = " << x << endl;
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cerr << *it << " = " << a << endl;
+	err(++it, args...);
+}
+/*---------------------------Codes-----------------------------*/
+
+
+int main() {
+
+
+    return 0;
+}
