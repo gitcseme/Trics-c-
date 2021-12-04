8730fda64e20e5642574e5193d6fd0c38e13f5c0
Md. Kawsarul Alam Shuvo <shuvo_CSE23@yahoo.com>
2021-12-05 01:10:08 +0600

    updated the template

--- template me.cpp
+++ template me.cpp
@@ -1,24 +1,36 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
 #define ff first
 #define ss second
+#define nline "\n"
+#define ll long long
 #define all(ar) ar.begin(), ar.end()
 #define FI freopen ("inp.txt", "r", stdin)
 #define FO freopen ("out.txt", "w", stdout)
-#define FOR(v) for(int i : v) {printf("%d ", i);} printf("\n");
-#define what_is(x) cerr << #x << " = " << x << endl;
-#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define FASTIO ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+#define debug(args...){string _s=#args;replace(_s.begin(),_s.end(),',',' ');stringstream _ss(_s);istream_iterator<string> _it(_ss);err(_it,args);}
 void err(istream_iterator<string> it) {}
-template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args) {
-	cerr << *it << " = " << a << endl;
-	err(++it, args...);
-}
+template<typename T,typename... Args> void err(istream_iterator<string> it,T a,Args... args){cerr<<*it<<" = "<<a<<"\n";err(++it,args...);}
+template<typename T> void _print(T _t){cout<<"[ ";for(auto _x:_t){cout<<_x<<" ";}cout<<" ]\n";}
+template<class T> void _print(map<T, T> x){for(auto i:x)cout<<i.ff<<" "<<i.ss<<"\n";}
 /*---------------------------Codes-----------------------------*/
 
+// LEET CODE
+//static const int _ = []() { ios::sync_with_stdio(false); cin.tie(NULL); return 0; }();
 
 int main() {
+    FASTIO;
+#ifdef DEBUG_LOCAL
+    FI;
+#endif
+    
 
+    
 
     return 0;
 }
