8a8b3ced2e9c720ad5c62062c2801f3277afd8ad
Md. Kawsarul Alam Shuvo <shuvo_CSE23@yahoo.com>
2021-12-26 12:22:25 +0600

    updated code template

--- template me.cpp
+++ template me.cpp
@@ -16,7 +16,7 @@ using namespace __gnu_pbds;
 #define debug(args...){string _s=#args;replace(_s.begin(),_s.end(),',',' ');stringstream _ss(_s);istream_iterator<string> _it(_ss);err(_it,args);}
 void err(istream_iterator<string> it) {}
 template<typename T,typename... Args> void err(istream_iterator<string> it,T a,Args... args){cerr<<*it<<" = "<<a<<"\n";err(++it,args...);}
-template<typename T> void _print(T _t){cout<<"[ ";for(auto _x:_t){cout<<_x<<" ";}cout<<" ]\n";}
+template<typename T> void _print(T _t){cout<<"[ ";for(auto _x:_t){cout<<_x<<" ";}cout<<"]\n";}
 template<class T> void _print(map<T, T> x){for(auto i:x)cout<<i.ff<<" "<<i.ss<<"\n";}
 /*---------------------------Codes-----------------------------*/
 
@@ -28,8 +28,6 @@ int main() {
 #ifdef DEBUG_LOCAL
     FI;
 #endif
-    
-
     
 
     return 0;
